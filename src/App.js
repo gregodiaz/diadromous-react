1b8b69af9ce809d64218b0a21ab32ed07e9e3825
gregodiaz <diazgregoriocruz@gmail.com>
2022-11-14 22:15:24 -0300

    Travel component with table headers.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Travels from './components/Travels';
-import Tickets from './components/Tickets';
+import Travels from './components/presentionals/Travels';
 
 export default function App() {
     return (
         <Router>
             <Routes>
                 <Route exact path='/' element={<Travels />} />
-                <Route exact path='tickets' element={<Tickets />} />
             </Routes>
         </Router>
     );
---
+++ src/components/presentionals/Travels/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default function Travels() {
+    return (
+        <div className='container'>
+            <table className='table table-dark'>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Price</th>
+                        <th>Departure City</th>
+                        <th>Departure date</th>
+                        <th>Arrival city</th>
+                        <th>Arrival date</th>
+                        <th>Total Passengers</th>
+                        <th>Remaining Tickets</th>
+                        <th>Show Forecast</th>
+                        <th>Buy Ticket</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
