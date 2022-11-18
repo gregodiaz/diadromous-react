34ae2e82cee359e4576786cfa341d589799aa66f
gregodiaz <diazgregoriocruz@gmail.com>
2022-11-18 15:36:14 -0300

    LoadingSpinner for travel detail and travel table.

--- src/components/functionals/Travels/index.jsx
+++ src/components/functionals/Travels/index.jsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from 'react';
 
+import LoadingSpinner from '../../presentionals/LoadingSpinner';
 import { getTravels } from '../../../services/TravelService';
 
 export default function Travels() {
+    const [isLoading, setIsLoading] = useState(false)
     const [travels, setTravels] = useState([])
 
     const getAllTravels = async () => {
         const allTravels = await getTravels()
+
         setTravels(allTravels);
+        setIsLoading(true);
     };
 
     const handleShow = event => { event.preventDefault() };
@@ -20,21 +24,23 @@ export default function Travels() {
     return (
         <tbody>
             {
-                travels.map(travel =>
-                    <tr key={travel.id}>
-                        <td>{travel.id}</td>
-                        <td>${travel.price}</td>
-                        {
-                            travel.cities.map(city => <td>{city.name}</td>)
-                        }
-                        <td>{travel.departure_date}</td>
-                        <td>{travel.arrival_date}</td>
-                        <td>{travel.total_passengers}</td>
-                        <td>{travel.available_passengers}</td>
-                        <td><button className='btn btn-outline-info' onClick={handleShow}>Show</button></td>
-                        <td><button className='btn btn-outline-warning' onClick={handleBuy}>Buy</button></td>
-                    </tr>
-                )
+                !isLoading ?
+                    <LoadingSpinner /> :
+                    travels.map(travel =>
+                        <tr key={travel.id}>
+                            <td>{travel.id}</td>
+                            <td>${travel.price}</td>
+                            {
+                                travel.cities.map(city => <td>{city.name}</td>)
+                            }
+                            <td>{travel.departure_date}</td>
+                            <td>{travel.arrival_date}</td>
+                            <td>{travel.total_passengers}</td>
+                            <td>{travel.available_passengers}</td>
+                            <td><button className='btn btn-outline-info' onClick={handleShow}>Show</button></td>
+                            <td><button className='btn btn-outline-warning' onClick={handleBuy}>Buy</button></td>
+                        </tr>
+                    )
             }
         </tbody>
     );
--- src/components/presentionals/TravelTable/index.jsx
+++ src/components/presentionals/TravelTable/index.jsx
@@ -6,25 +6,23 @@ export default function TravelTable() {
 
     return (
         <div className='container'>
-            <div className='table table-responsive-xl'>
-                <table className='table table-striped table-dark table-hover'>
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Price</th>
-                            <th>Departure City</th>
-                            <th>Arrival city</th>
-                            <th>Departure date</th>
-                            <th>Arrival date</th>
-                            <th>Total Passengers</th>
-                            <th>Remaining Tickets</th>
-                            <th>Show Forecast</th>
-                            <th>Buy Ticket</th>
-                        </tr>
-                    </thead>
-                    <Travels />
-                </table>
-            </div>
+            <table className='table table-striped table-dark table-hover'>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Price</th>
+                        <th>Departure City</th>
+                        <th>Arrival city</th>
+                        <th>Departure date</th>
+                        <th>Arrival date</th>
+                        <th>Total Passengers</th>
+                        <th>Remaining Tickets</th>
+                        <th>Show Forecast</th>
+                        <th>Buy Ticket</th>
+                    </tr>
+                </thead>
+                <Travels />
+            </table>
         </div >
     );
 };
